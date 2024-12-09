b26e84a4637d42382434d9f12c872ec9f20145a4
kiichinakai <setoutijack@gmail.com>
2024-12-09 19:45:54 +0900

    regiterの作成

--- frontend/src/components/page/Login.tsx
+++ frontend/src/components/page/Login.tsx
@@ -1,7 +1,10 @@
-import { Button, Card, Center, Input, Stack } from "@chakra-ui/react"
+import { Button, Card, Center, Input, Stack, Text } from "@chakra-ui/react"
 import { Field } from "../ui/field"
+import { PasswordInput } from "../ui/password-input"
+import { useNavigate } from "react-router-dom"
 
 export const Login = (() => {
+    const navigate = useNavigate();
     return (
         <Center h="800px">
             <Card.Root w="md">
@@ -14,9 +17,10 @@ export const Login = (() => {
                             <Input />
                         </Field>
                         <Field label="パスワード">
-                            <Input />
+                            <PasswordInput />
                         </Field>
                     </Stack>
+                    <Text mt="5" onClick={(() => { navigate("/register") })} cursor="pointer">新規登録はこちら</Text >
                 </Card.Body>
                 <Card.Footer justifyContent="flex-end">
                     <Button variant="outline">Cancel</Button>
