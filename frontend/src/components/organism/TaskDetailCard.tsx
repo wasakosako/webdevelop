82067786576567ed6c14a4eee8fe1b0acd325a5b
hogehoge <hogehoge@gmail.com>
2024-12-09 13:05:17 +0900

    temp

--- frontend/src/components/organism/TaskDetailCard.tsx
+++ frontend/src/components/organism/TaskDetailCard.tsx
@@ -1,12 +1,13 @@
-import { Button, Card, Center, Text } from "@chakra-ui/react"
+import { Button, Card, Center, Spinner, Text } from "@chakra-ui/react"
 import { useNavigate } from "react-router-dom"
 import { tasktypedetail } from "../../types/atoms";
-import { memo, useState } from "react";
-import axios from "axios";
+import { memo } from "react";
+import { usePutTask } from "../../hooks/putTask";
 
 export const TaskDetailCard = memo((props: tasktypedetail) => {
     const navigate = useNavigate();
-    const [putid, setputid] = useState(undefined);
+    const { putid,setputid,loading,putTask}=usePutTask();
+
     return (
 
         <Card.Root width="320px">
@@ -19,13 +20,11 @@ export const TaskDetailCard = memo((props: tasktypedetail) => {
             <Card.Footer justifyContent="flex-end">
                 <Button variant="outline" onClick={(() => navigate(-1))}>戻る</Button>
                 <Button onClick={(() => {
-                    axios.put(`https://jsonplaceholder.typicode.com/todos/${props.id}`).then((res) => {
-                        setputid(res.data.id)
-                        console.log(res.data);
-                    }).catch((err) => { setputid(err) })
+                    setputid(props.id);
+                    putTask();
                 })}>完了にする</Button>
             </Card.Footer>
-            <Center mb="20px">{putid ? <Text ml="85px">{`${putid}が完了しました`}</Text> : ""}</Center>
+            <Center mb="20px">{putid==undefined? <></>:loading ? <Spinner/>:<Text ml="85px">{`${putid}を完了にしました`}</Text> }</Center>
         </Card.Root>
 
     )
